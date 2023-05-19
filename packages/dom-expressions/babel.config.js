3524e88938543cfefe7b9fda22d715f6330a405e
Nick Etson <nick.etson@gmail.com>
2023-05-19 15:52:01 -0400

    Updates test config

--- packages/dom-expressions/babel.config.js
+++ packages/dom-expressions/babel.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   presets: [["@babel/preset-env", { targets: { node: "current" } }]],
   plugins: [
-    [
-      "babel-plugin-transform-rename-import",
-      {
-        original: "rxcore",
-        replacement: __dirname + "/core"
-      }
-    ],
-    ["babel-plugin-jsx-dom-expressions", { moduleName: "./custom", generate: "universal" }]
+    ["babel-plugin-jsx-dom-expressions",
+     {
+       moduleName: "./custom",
+
+       delegateEvents: false,
+       effectWrapper: false,
+       memoWrapper: false,
+       wrapConditionals: false,
+     }
+    ]
   ]
 };
--- packages/dom-expressions/test/transform-dom.js
+++ packages/dom-expressions/test/transform-dom.js
@@ -3,13 +3,14 @@ const babelJest = require("babel-jest");
 module.exports = babelJest.createTransformer({
   presets: [["@babel/preset-env", { targets: { node: "current" } }]],
   plugins: [
-    [
-      "babel-plugin-transform-rename-import",
-      {
-        original: "rxcore",
-        replacement: __dirname + "/core"
-      }
-    ],
-    ["babel-plugin-jsx-dom-expressions", { moduleName: "../../src/client" }]
+    ["babel-plugin-jsx-dom-expressions",
+     {
+       moduleName: "../../src/client",
+
+       delegateEvents: false,
+       effectWrapper: false,
+       memoWrapper: false,
+       wrapConditionals: false,
+     }]
   ]
 });
